c289954097b7314aea126e213284d4f3d04fbd41
sofiaperezm <sofi.prz.m@gmail.com>
2021-10-21 22:09:57 -0500

    added user interaction

---
+++ app.js
@@ -0,0 +1,51 @@
+const pageViews = document.querySelector(".pageviews-count");
+const pageViewsInput = document.querySelector("#pageviews-input");
+const priceOutput = document.querySelector(".price");
+const billingToggle = document.querySelector("#billing-toggle");
+
+const priceRanges = {"10K": 8, "50K": 12, "100K": 16, "500K": 24, "1M": 36};
+const YEARLY_DISCOUNT = 0.25;
+
+pageViewsInput.addEventListener("input", handlePageViewsCount);
+billingToggle.addEventListener("change", calculatePrice);
+
+function getPageViews(pageViewsCount) {
+    if (pageViewsCount <= 10000) {
+        return "10K";
+    }
+    if (pageViewsCount > 10000 && pageViewsCount <= 50000) {
+        return "50K";
+    }
+    if (pageViewsCount > 50000 && pageViewsCount <= 100000) {
+        return "100K";
+    }
+    if (pageViewsCount > 100000 && pageViewsCount <= 500000) {
+        return "500K";
+    }
+    if (pageViewsCount > 500000 && pageViewsCount <= 1000000) {
+        return "1M";
+    }
+}
+
+function handlePageViewsCount() {
+    const pageViewsCount = pageViewsInput.value;
+    const pageViewsOutput = getPageViews(pageViewsCount);
+
+    pageViews.innerHTML = `${pageViewsOutput} PAGEVIEWS`;
+
+    calculatePrice();
+};
+
+function calculatePrice() {
+    const billingToggleChecked = billingToggle.checked;
+    const pageViewsCount = pageViewsInput.value;
+    const pageViewsOutput = getPageViews(pageViewsCount);
+    const pricing = priceRanges[pageViewsOutput];
+    
+    if (billingToggleChecked) {
+        const yearlyPrincing = pricing - pricing * YEARLY_DISCOUNT;
+        priceOutput.innerHTML = `$${yearlyPrincing}/month`;
+    } else {
+        priceOutput.innerHTML = `$${pricing}/month`;
+    }
+};
